77ab444d12b05ce1f45aa82e6855e20f9f4a789e
AristideExe <aristidepr@gmail.com>
2026-05-03 00:14:28 +0200

    feat: add player name and money above their head

--- Code/Player/PlayerManager.cs
+++ Code/Player/PlayerManager.cs
@@ -4,7 +4,14 @@ namespace Sandbox.Player;
 
 public sealed class PlayerManager : Component
 {
+	[Property] private PlayerInfosUI PlayerInfosUi;
 	[RequireComponent] private Wallet Wallet { get; set;  }
 	
 	public float Money => Wallet.Money;
+
+	protected override void OnAwake()
+	{
+		base.OnAwake();
+		PlayerInfosUi.Player = this;
+	}
 }
